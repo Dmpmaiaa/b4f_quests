35c160dc015444a6ef73683baf18b7c1579dbad4
Diogo Manuel Peixoto Maia <diogompmaia@gmail.com>
2023-04-24 08:37:41 +0100

    quests

--- recursao-arrayNa0.js
+++ recursao-arrayNa0.js
@@ -1,17 +1,21 @@
-function arrayDeNAZero(n){
-    if (n === 0) {
-        return [0]
-    }
-   return [n].concat(arrayDeNAZero(n-1))
-   // return [n, ...arrayDeNAZero(n - 1)]
- }
+// function arrayDeNAZero(n){
+//     if (n === 0) {
+//         return [0]
+//     }
+//    return [n].concat(arrayDeNAZero(n-1))
+//    // return [n, ...arrayDeNAZero(n - 1)]
+//  }
 
- console.log(arrayDeNAZero(4));
+//  console.log(arrayDeNAZero(4));
 
- // [4,3,2,1,0]
+//  // [4,3,2,1,0]
 
- // [4] concat(n-1)
- // [4, n-1].concat(n-1)
- // [4, n-1, n-1].concat(n-1)
- // [4, n-1, n-1, 1].concat(1)
- // [4, n-1 , n-1, n-1, 0].concat([0])
+//  // [4] concat(n-1)
+//  // [4, n-1].concat(n-1)
+//  // [4, n-1, n-1].concat(n-1)
+//  // [4, n-1, n-1, 1].concat(1)
+//  // [4, n-1 , n-1, n-1, 0].concat([0])
+
+
+
+
